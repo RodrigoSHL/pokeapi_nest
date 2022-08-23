e9d6212b080bd0480230545136d48ca00f766e4f
RodrigoSHL <shl.catalan@gmail.com>
2022-08-23 16:25:07 -0400

    add: dto pokemon created

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePokemonDto {}
+import {
+  IsNumber,
+  IsString,
+  MinLength,
+  Min,
+  IsPositive,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+  @IsNumber()
+  @Min(0)
+  @IsPositive()
+  no: number;
+}
