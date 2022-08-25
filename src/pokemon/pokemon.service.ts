ec426e5291337de4382aa121e0b32408b5078006
RodrigoSHL <shl.catalan@gmail.com>
2022-08-25 13:05:17 -0400

    add: findOne method added
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,7 +5,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -39,14 +39,26 @@ export class PokemonService {
     return this.pokemonModel.find();
   }
 
-  findOne(id: string) {
-    const poke = this.pokemonModel.findById(id);
-    if (!poke) throw new NotFoundException();
-    return poke;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toUpperCase() });
+    }
+    if (!pokemon)
+      throw new NotFoundException(
+        `Pokemon with id, name or no "${term}" not found`,
+      );
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    return updatePokemonDto;
   }
 
   remove(id: number) {
