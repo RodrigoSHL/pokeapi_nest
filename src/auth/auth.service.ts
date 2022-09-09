c6bd8f761c8427f56e46064a8b2ad04a860b7872
RodrigoSHL <shl.catalan@gmail.com>
2022-09-08 22:49:25 -0400

    feat: add token in response in register and login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,17 +10,28 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private readonly userModel: Model<User>,
+    private readonly jwtService: JwtService,
   ) {}
+
   async register(createUserDto: CreateUserDto) {
     try {
-      const user = await this.userModel.create(createUserDto);
-      return user;
+      const { password, ...userData } = createUserDto;
+      const user = await this.userModel.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+      return {
+        ...userData,
+        token: this.getJwtToken({ id: user._id }),
+      };
     } catch (error) {
       this.handleException(error);
     }
@@ -30,11 +41,22 @@ export class AuthService {
     const { password, email } = loginUserDto;
     const user = await this.userModel
       .findOne({ email: email })
-      .select({ email: 1, password: 1 });
+      .select('email password');
     if (!user) {
       throw new UnauthorizedException('User not found');
     }
-    return user;
+    if (!bcrypt.compare(password, user.password)) {
+      throw new UnauthorizedException('Credentials not valid');
+    }
+    return {
+      ...loginUserDto,
+      token: this.getJwtToken({ id: user._id }),
+    };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
   private handleException(error: any) {
