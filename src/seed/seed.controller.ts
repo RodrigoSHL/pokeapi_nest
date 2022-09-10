c92fdf051c6d92ea657b2ef36f94e8cd066beb26
RodrigoSHL <shl.catalan@gmail.com>
2022-09-09 22:24:37 -0400

    feat: import auth module in seed controller

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,12 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
 import { SeedService } from './seed.service';
 
 @Controller('seed')
@@ -14,6 +8,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  @Auth(ValidRoles.superUser)
   executeSeed() {
     return this.seedService.executeSeed();
   }
