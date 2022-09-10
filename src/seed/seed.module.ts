94194d6e5ed7e1b80f9f4b5aa27370f407916f2e
RodrigoSHL <shl.catalan@gmail.com>
2022-09-09 22:25:13 -0400

    feat: import auth module in seed module

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,9 +3,10 @@ import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { HttpModule } from '@nestjs/axios';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [HttpModule, PokemonModule],
+  imports: [HttpModule, PokemonModule, AuthModule],
   controllers: [SeedController],
   providers: [SeedService],
 })
