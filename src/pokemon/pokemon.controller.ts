d54e534f49d5619bfdec8859b46a88f74fb13e49
RodrigoSHL <shl.catalan@gmail.com>
2022-09-09 22:28:00 -0400

    feat: import auth in pokemon controller

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -15,28 +15,34 @@ import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Post()
+  @Auth()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createPokemonDto: CreatePokemonDto) {
     return this.pokemonService.create(createPokemonDto);
   }
 
   @Get()
+  @Auth()
   findAll(@Query() paginationDto: PaginationDto) {
     return this.pokemonService.findAll(paginationDto);
   }
 
   @Get(':term')
+  @Auth()
   findOne(@Param('term') term: string) {
     return this.pokemonService.findOne(term);
   }
 
   @Patch(':term')
+  @Auth()
   update(
     @Param('term') term: string,
     @Body() updatePokemonDto: UpdatePokemonDto,
@@ -45,6 +51,7 @@ export class PokemonController {
   }
 
   @Delete(':term')
+  @Auth(ValidRoles.admin)
   remove(@Param('term', ParseMongoIdPipe) term: string) {
     return this.pokemonService.remove(term);
   }
