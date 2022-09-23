c00e6306b6e7186429b9554060fee5b9d3afa76e
RodrigoSHL <shl.catalan@gmail.com>
2022-09-23 17:51:02 -0300

    feat: add id parameter in updated method

--- src/event/dto/update-event.dto.ts
+++ src/event/dto/update-event.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString } from 'class-validator';
 import { CreateEventDto } from './create-event.dto';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+  @IsString()
+  _id?: string;
+}
