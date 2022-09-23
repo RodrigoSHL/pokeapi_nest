a1168264acd540389c60f8ed4ca647c679665808
RodrigoSHL <shl.catalan@gmail.com>
2022-09-23 17:48:21 -0300

    feat: add takeTime controller and the authentication logic was modified (only decorators position)

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -13,32 +13,50 @@ import { UpdateEventDto } from './dto/update-event.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { ClientTakeTimeDto } from './dto/client-take-time.dto';
 
-@Auth()
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
+  @Auth()
   @Post()
   create(@Body() createEventDto: CreateEventDto, @GetUser() user: User) {
     return this.eventService.create(createEventDto, user);
   }
 
+  @Patch('/take/:id')
+  takeTime(
+    @Param('id') id: string,
+    @Body() clientTakeTimeDto: ClientTakeTimeDto,
+  ) {
+    return this.eventService.takeTime(clientTakeTimeDto, id);
+  }
+
+  @Auth()
   @Get()
   findAll() {
     return this.eventService.findAll();
   }
 
+  @Auth()
   @Get('/user')
-  findAllByUserId(@GetUser() user: User) {
-    return this.eventService.findEventByUserId(user);
+  findAllByUser(@GetUser() user: User) {
+    return this.eventService.findEventByUser(user);
   }
 
+  @Get('/user/:id')
+  findAllByUserId(@Param('id') id: string) {
+    return this.eventService.findEventByUserId(id);
+  }
+
+  @Auth()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.eventService.findOne(id);
   }
 
+  @Auth()
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -48,6 +66,7 @@ export class EventController {
     return this.eventService.update(id, updateEventDto, user);
   }
 
+  @Auth()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.eventService.remove(id);
